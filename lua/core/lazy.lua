99d70cb7d7ba4ce1b47017c935553952aaa5394c
Mitesh Soni <smiteshhc@gmail.com>
2025-02-08 15:56:19 +0000

    Updated LuaSnip options
    Added `make install_regexpr`

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -97,6 +97,7 @@ lazy.setup({
         {
             "L3MON4D3/LuaSnip",
             version = "v2.3",
+            run = "make install_jsregexp",
             config = function()
                 local ls = require('luasnip')
                 ls.config.setup({
